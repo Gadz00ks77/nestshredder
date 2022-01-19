94e5d7c54c45183e1810e45453f05fcf2aa1d67d
Chris Woodward <chriswoodward77@googlemail.com>
2022-01-19 16:29:55 +0000

    Updated readme and refined argument names

--- nestshredder/__init__.py
+++ nestshredder/__init__.py
@@ -4,29 +4,29 @@ import json
 import sys
 from nestshredder.pyshred_core import _shred_recursive
 
-def shred_json(filepath,targetpath,objname):
+def shred_json(source_file_path,target_folder_path,object_name):
 
-    if os.path.exists(targetpath) == False:
+    if os.path.exists(target_folder_path) == False:
         is_success = 'Target Path Does Not Exist'
         print("1:Error - " + is_success )
         return "1:Error - " + is_success   
 
-    if os.path.exists(filepath) == False:
+    if os.path.exists(source_file_path) == False:
         is_success = 'Source Path Does Not Exist'
         print("1:Error - " + is_success )
         return "1:Error - " + is_success   
 
     try:
-        json_df = pd.read_json(filepath)
-        is_success = _shred_recursive(json_df,targetpath,objname,objname,objname)
+        json_df = pd.read_json(source_file_path)
+        is_success = _shred_recursive(json_df,target_folder_path,object_name,object_name,object_name)
     except Exception as e:        
         if str(e) == 'If using all scalar values, you must pass an index':
             new_list = []
-            with open(filepath) as json_file:
+            with open(source_file_path) as json_file:
                 data = json.load(json_file)
                 new_list.append(data)
                 json_df = pd.DataFrame.from_dict(new_list)
-                is_success = _shred_recursive(json_df,targetpath,objname,objname,objname)
+                is_success = _shred_recursive(json_df,target_folder_path,object_name,object_name,object_name)
         else:
             is_success = str(e)
 
@@ -36,21 +36,21 @@ def shred_json(filepath,targetpath,objname):
     else: 
         return "0:OK"
 
-def shred_parquet(filepath,targetpath,objname):
+def shred_parquet(source_file_path,target_folder_path,object_name):
 
-    if os.path.exists(targetpath) == False:
+    if os.path.exists(target_folder_path) == False:
         is_success = 'Target Path Does Not Exist'
         print("1:Error - " + is_success )
         return "1:Error - " + is_success   
 
-    if os.path.exists(filepath) == False:
+    if os.path.exists(source_file_path) == False:
         is_success = 'Source Path Does Not Exist'
         print("1:Error - " + is_success )
         return "1:Error - " + is_success   
 
     try:
-        json_df = pd.read_parquet(filepath)
-        is_success = _shred_recursive(json_df,targetpath,objname,objname,objname)
+        json_df = pd.read_parquet(source_file_path)
+        is_success = _shred_recursive(json_df,target_folder_path,object_name,object_name,object_name)
     except Exception as e:        
         is_success = str(e)
 
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # Python Nest Shredder
 ## Turn nested data into relational files!
 
-Nest Shredder is a pandas-wrapper utility for converting nested JSON data into relational Parquets, typically for onward consumption into a relational database (where nested data may become less immediately useful).
+Nest Shredder is a pandas-wrapper utility for converting nested JSON or Parquet data into relational "flat" Parquet files, typically for onward consumption into a relational database (where nested data may become less immediately useful).
 
 ## Features
 
@@ -14,7 +14,10 @@ Nest Shredder is a pandas-wrapper utility for converting nested JSON data into r
 
 - If you shred the same JSON object twice that has a nested array of objects I don't guarantee the order for each shred-time (but the ids will be valid for the run). Get yourself a key on that object! :) 
 - No CSV at the moment. Simple add, will do later.
-- No compression on the Parquet as standard. Will add later.
+- No compression on the output Parquet as standard. Will add later.
+- Support for other Parquet reader libraries. May be later.
+- Something to identify what "batch" this was when you called the shredder and adds that to the data. Will add later.
+- Represent the full path in the parquet output to account for people naming child objects the same thing repeatedly. Will add later and burst into tears. Model your data properly.
 
 ## Tech
 
@@ -26,6 +29,24 @@ Nest Shredder uses a couple of open source projects to work properly:
 ## Installation
 
 PyPi via PipEnv or Pip itself. Up to you!
+
+## Usage
+
+The module exposes two functions at the moment;
+1. shred_json
+2. shred parquet
+
+Both accept:
+source_file_path - the source file path (e.g. './examples/test.json')
+target_folder_path - the path where you would like your flattened / unnested outputs. New folders will be created in here, using:
+object_name - a simple string that you can use to identify the overall object represented by your data (e.g. Customers or Addresses). One word only please.
+
+e.g.
+```
+import nestshredder as ns
+
+ns.shred_json('./examples/vsimple_example.json','./target','example')
+```
 
 ## License
 
